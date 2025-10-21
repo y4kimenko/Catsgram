b7933f322e2606cdda56a402e21de3d40ec1c407
Nikita Yakimenko <nikisan213@mail.ru>
2025-10-22 00:58:58 +0300

    Добавление валидации для User

--- src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
+++ src/main/java/ru/yandex/practicum/catsgram/service/UserService.java
@@ -21,6 +21,18 @@ public class UserService {
 
     public User create(User user) {
         validateUser(user);
+        if (user.getEmail() == null) {
+            throw new ConditionsNotMetException("Email не может быть пустым");
+        }
+        if (users.containsValue(user)) {
+            throw new ConditionsNotMetException("Этот Email уже используется");
+        }
+        if (user.getUsername() == null) {
+            user.setUsername(user.getEmail());
+        }
+        if (user.getPassword() == null) {
+            throw new ConditionsNotMetException("Password не может быть пустым");
+        }
 
         user.setId(getNextId());
         user.setRegistrationDate(Instant.now());
@@ -32,14 +44,28 @@ public class UserService {
         if (newUser.getId() == null) {
             throw new ConditionsNotMetException("Id должен быть указан");
         }
+
+
         if (users.containsKey(newUser.getId())) {
             User oldUser = users.get(newUser.getId());
+
+            if (users.containsValue(newUser) && newUser.getEmail().equals(oldUser.getEmail())) {
+                throw new ConditionsNotMetException("Этот Email уже используется");
+            }
+
             validateUser(newUser);
 
-            oldUser.setUsername(newUser.getUsername());
-            oldUser.setEmail(newUser.getEmail());
-            oldUser.setPassword(newUser.getPassword());
-            oldUser.setRegistrationDate(Instant.now());
+            if (newUser.getEmail() != null) {
+                oldUser.setEmail(newUser.getEmail());
+            }
+            if (newUser.getUsername() != null) {
+                oldUser.setUsername(newUser.getUsername());
+            }
+            if (newUser.getPassword() != null) {
+                oldUser.setPassword(newUser.getPassword());
+            }
+
+            oldUser.setRegistrationDate(newUser.getRegistrationDate());
 
             return oldUser;
         }
@@ -47,13 +73,14 @@ public class UserService {
     }
 
     private void validateUser(User user) {
-        if (user.getEmail() == null || user.getEmail().isBlank()) {
+
+        if (user.getEmail().isBlank()) {
             throw new ConditionsNotMetException("Email не может быть пустым");
         }
-        if (user.getUsername() == null || user.getUsername().isBlank()) {
+        if (user.getUsername().isBlank()) {
             user.setUsername(user.getEmail());
         }
-        if (user.getPassword() == null || user.getPassword().isBlank()) {
+        if (user.getPassword().isBlank()) {
             throw new ConditionsNotMetException("Password не может быть пустым");
         }
     }
